337f566cff174da6b4dca4a49933f46c344e2da8
David Winings <wisp558@gmail.com>
2017-04-21 17:16:55 -0400

    More boilerplate.

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD38Runner {
+  public class GameManager : MonoBehaviour {
+    public static GameManager _instance = null;
+
+    // Use this for initialization
+    void Start() {
+      if (!_instance) {
+        _instance = this;
+      } else {
+        GameObject.DestroyImmediate(this);
+      }
+    }
+
+    void Update() {
+      //TODO: Write entire game.
+    }
+  }
+}
