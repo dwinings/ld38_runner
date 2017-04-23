28d2f03de3c91c5b503136cf0e1d3a4c3af2b93c
David Winings <wisp558@gmail.com>
2017-04-23 12:37:20 -0400

    Neon floor tiles!

---
+++ Assets/Level Chunks/neon_tileset.tsx
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="neon_tileset" tilewidth="128" tileheight="128" tilecount="225" columns="15">
+ <image source="../Viz/ld38_spritesheet.png" width="1920" height="1920"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="oj_nw"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="oj_n"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="oj_ne"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="type" value="m_nw"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="type" value="m_n"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="type" value="m_ne"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="type" value="b_nw"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="type" value="b_n"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="type" value="b_ne"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="type" value="oj_w"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="type" value="oj_c"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="type" value="oj_e"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="type" value="m_w"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="type" value="m_c"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="type" value="m_e"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="type" value="b_w"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="type" value="b_c"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="type" value="b_e"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="type" value="oj_sw"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="type" value="oj_s"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="type" value="oj_sw"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="type" value="m_sw"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="type" value="m_s"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="type" value="m_se"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="type" value="b_sw"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="type" value="b_s"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="type" value="b_se"/>
+  </properties>
+ </tile>
+</tileset>
