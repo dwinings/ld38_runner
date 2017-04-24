58ec7e9d05bb29bbdadff86abdd66b9a6303bf11
David Winings <wisp558@gmail.com>
2017-04-24 08:22:28 -0400

    Credits screen tho.

---
+++ Assets/Scripts/Menu/CreditsScroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LD38Runner {
+  public class CreditsScroller : MonoBehaviour {
+    public float scrollSpeed;
+
+    void Update() {
+      var rect = GetComponent<RectTransform>();
+      rect.Translate(0, scrollSpeed*Time.deltaTime, 0);
+
+      if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Q) || Mathf.Abs(transform.position.y) > rect.rect.height) {
+        SceneManager.LoadScene(0); 
+      }
+    }
+  }
+}
--- Assets/Scripts/Menu/LoadLevel.cs
+++ Assets/Scripts/Menu/LoadLevel.cs
@@ -5,19 +5,12 @@ using UnityEngine.SceneManagement;
 
 namespace LD38Runner {
   public class LoadLevel : MonoBehaviour {
-
-  	// Use this for initialization
-  	void Start () {
-  		
-  	}
-  	
-  	// Update is called once per frame
-  	void Update () {
-  		
-  	}
-
     public void LoadLevel1() {
       SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel2() {
+      SceneManager.LoadScene(2);
     }
   }
 }
